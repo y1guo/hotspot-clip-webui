4c901b9fe755c976a765684e725704d559461ad9
y1guo <guoyi0328@gmail.com>
2023-03-31 06:33:16 -0700

    add dplayer; video working

--- src/App.tsx
+++ src/App.tsx
@@ -36,12 +36,11 @@ function FileInput(props: any) {
       return file.type === "text/xml";
     });
     // update video and danmaku arraies in appData
-    props.setAppData({
-      ...props.appData,
+    props.setAppData((appData: AppData) => ({
+      ...appData,
       videoFileArray: videoFileArray,
       danmakuFileArray: danmakuFileArray,
-      test: "changed",
-    });
+    }));
   };
   return (
     <MuiFileInput multiple onChange={handleChangeFileArray} hideSizeText />
@@ -49,9 +48,17 @@ function FileInput(props: any) {
 }
 
 function FilePanelItem(props: any) {
+  // set selected video on click
+  const handleClick = () => {
+    props.setAppData((appData: AppData) => ({
+      ...appData,
+      selectedVideo: props.file,
+    }));
+  };
+
   return (
     <ListItem>
-      <ListItemButton>
+      <ListItemButton onClick={handleClick}>
         <ListItemText
           primary={props.file.name + " (" + props.file.size + " B)"}
         />
@@ -64,61 +71,63 @@ function FilePanel(props: any) {
   return (
     <List>
       {props.fileArray.map((file: File, i: number) => (
-        <FilePanelItem file={file} key={i} />
+        <FilePanelItem file={file} key={i} setAppData={props.setAppData} />
       ))}
     </List>
   );
 }
 
+function VideoPlayer(props: any) {
+  if (document.getElementById("dplayer")) {
+    const dp = new DPlayer({
+      container: document.getElementById("dplayer"),
+      screenshot: true,
+      video: {
+        // url: "92613_20230329_095623_010529.mp4",
+        url: props.video ? URL.createObjectURL(props.video) : "",
+      },
+      // danmaku: {
+      //   id: "demo",
+      //   api: "https://api.prprpr.me/dplayer/",
+      // },
+    });
+  }
+  return <div id="dplayer"></div>;
+}
+
 interface AppData {
   videoFileArray: File[];
   danmakuFileArray: File[];
-  test: string;
+  selectedVideo: File | null;
+  selectedDanmaku: File | null;
 }
 
 export default function App() {
   const [appData, setAppData] = React.useState<AppData>({
     videoFileArray: [],
     danmakuFileArray: [],
-    test: "test",
+    selectedVideo: null,
+    selectedDanmaku: null,
   });
-
-  // const dp = new DPlayer({
-  //   container: document.getElementById("dplayer"),
-  //   // screenshot: true,
-  //   video: {
-  //     url: "demo.mp4",
-  //     // pic: "demo.jpg",
-  //     // thumbnails: "thumbnails.jpg",
-  //   },
-  //   // danmaku: {
-  //   //   id: "demo",
-  //   //   api: "https://api.prprpr.me/dplayer/",
-  //   // },
-  // });
 
   return (
     <Container maxWidth="sm">
       <Box>
         Select Videos (flv, mp4) and Danmaku Files (xml):
-        <FileInput appData={appData} setAppData={setAppData} />
-        {/* <input
-          type="file"
-          multiple
-          onChange={(e) => {
-            // if e.currentTarget.files !== null {
-            //   handleChangeFileArray([...e.currentTarget.files]);            }
-            if (e.currentTarget.files) {
-              for (let i = 0; i < e.currentTarget.files?.length; i++) {
-                console.log(e.currentTarget.files?.item(i));
-              }
-            }
-          }}
-        /> */}
+        <FileInput setAppData={setAppData} />
       </Box>
       <Box>
-        <FilePanel fileArray={appData.videoFileArray} />
-        <FilePanel fileArray={appData.danmakuFileArray} />
+        <FilePanel fileArray={appData.videoFileArray} setAppData={setAppData} />
+        <FilePanel
+          fileArray={appData.danmakuFileArray}
+          setAppData={setAppData}
+        />
+      </Box>
+      <Box>
+        <VideoPlayer
+          video={appData.selectedVideo}
+          danmaku={appData.selectedDanmaku}
+        />
       </Box>
       <Copyright />
     </Container>
