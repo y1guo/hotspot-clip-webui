68edc64a70cdb2c6a47fa28d9b6e1640bc962ee8
y1guo <guoyi0328@gmail.com>
2023-03-31 02:48:05 -0700

    add mui-file-input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import { Button } from '@mui/material';
+import { MuiFileInput } from 'mui-file-input'
 
-function App() {
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/y1guo/hotspot-clip-webui">
+        hotspot-clip-webui
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  const [appData, setAppData] = React.useState({});
+  const [VideoFileArray, setVideoFileArray] = React.useState<File[]>([])
+  const [xmlFileArray, setXmlFileArray] = React.useState<File[]>([])
+
+  const handleChangeVideoFileArray = (newValue: File[]) => {
+    setVideoFileArray(newValue)
+  }
+
+  const handleChangeXmlFileArray = (newValue: File[]) => {
+    setXmlFileArray(newValue)
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Box>
+        <MuiFileInput multiple value={VideoFileArray} onChange={handleChangeVideoFileArray} hideSizeText/>
+        <MuiFileInput multiple value={xmlFileArray} onChange={handleChangeXmlFileArray} hideSizeText/>
+      </Box>
+      <Copyright />
+    </Container>
+  );
+}
+
